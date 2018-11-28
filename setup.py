016058052f1999fd2100028214311794fe650018
Siddharth <sidjain1412@gmail.com>
2018-11-28 17:13:14 +0530

    Requirements, updated version with long_description

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='df_overview',
-      version='0.1',
+      version='0.3',
       description='Quick overview of a dataframe, for data analysis.',
       url='http://github.com/SidJain1412/df_overview',
       author='Siddharth Jain',
@@ -14,5 +14,7 @@ setup(name='df_overview',
           'numpy>=1.14.2',
           'seaborn>=0.8.1',
           'scipy>=1.0.0',
-		  'matplotlib>=2.1.2',
-      ],)
+          'matplotlib>=2.1.2',
+      ],
+      long_description='A python package to do basic data analysis of your dataframe with just one line of code',
+      )
